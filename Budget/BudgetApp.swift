f548db1c82280b9047df16c1bbed8dbcbc950798
Raul Lamas <raul.lamas.b@me.com>
2024-01-10 13:39:08 -0500

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Raul Lamas on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
