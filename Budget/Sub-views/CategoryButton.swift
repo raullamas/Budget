efeb4eb805e70a491e0533c128f059969b2939cd
Raul Lamas <raul.lamas.b@me.com>
2024-01-11 07:51:37 -0500

    transactionContent: Add CategoryButton sub-view

---
+++ Budget/Sub-views/CategoryButton.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryButton.swift
+//  Budget
+//
+//  Created by Raul Lamas on 1/10/24.
+//
+
+import SwiftUI
+
+struct CategoryButton: View {
+    let category: Transaction.Category,
+        action: () -> Void
+    var selected: Bool = false
+    
+    var body: some View {
+        Button(action: action) {
+            VStack {
+                CategoryView(category: category, highlighted: selected)
+                
+                Text(category.rawValue)
+                    .font(.headline)
+                    .foregroundColor(selected ? .primary : .secondary)
+            }
+        }
+    }
+}
+
+#Preview {
+    CategoryButton(
+        category: .utilities,
+        action: { Void() },
+        selected: true
+        
+    )
+}
