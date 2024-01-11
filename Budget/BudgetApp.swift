ce8c76fe5ba7fb2d4866db4ea570e530dcd6fcce
Raul Lamas <raul.lamas.b@me.com>
2024-01-11 13:58:04 -0500

    main: Add stateController observable

--- Budget/BudgetApp.swift
+++ Budget/BudgetApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct BudgetApp: App {
+    @StateObject private var stateController = StateController()
+    
     var body: some Scene {
         WindowGroup {
             BudgetView()
+                .environmentObject(stateController)
         }
     }
 }
