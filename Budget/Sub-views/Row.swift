cf92a4766fab9ff9681aee91e33c027e308538bc
Raul Lamas <raul.lamas.b@me.com>
2024-01-10 19:59:46 -0500

    accountView: Add row sub-view

---
+++ Budget/Sub-views/Row.swift
@@ -0,0 +1,18 @@
+//
+//  Row.swift
+//  Budget
+//
+//  Created by Raul Lamas on 1/10/24.
+//
+
+import SwiftUI
+
+struct Row: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Row()
+}
