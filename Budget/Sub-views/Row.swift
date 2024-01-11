6e09d71640eaad9ed0e5ebdebfebabe0acc10661
Raul Lamas <raul.lamas.b@me.com>
2024-01-10 20:16:42 -0500

    accountView: Update row sub-view

--- Budget/Sub-views/Row.swift
+++ Budget/Sub-views/Row.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct Row: View {
+    let transaction: Transaction
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 16.0) {
+            CategoryView(category: transaction.category)
+            
+            VStack(alignment: .leading, spacing: 4.0) {
+                Text(transaction.category.rawValue)
+                    .font(.headline)
+                
+                Text(transaction.description)
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+            
+            Spacer()
+            
+            VStack(alignment: .trailing, spacing: 4.0) {
+                Text(transaction.amount.currencyFormat)
+                    .font(.headline)
+                    .foregroundStyle(color(for: transaction.amount))
+                
+                Text(transaction.date.transactionFormat)
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .padding(.vertical)
+    }
+    
+    func color(for amount: Int) -> Color {
+        amount > 0 ? .blue: .primary
     }
 }
 
 #Preview {
-    Row()
+    Row(transaction: Transaction(
+        amount: 200_000,
+        date: Date(),
+        description: "Salary",
+        category: .income
+    ))
 }
