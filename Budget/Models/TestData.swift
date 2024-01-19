a7af38c9005ba878c6ecd5ebf7fd838a2b6d1e46
Raul Lamas <raul.lamas.b@me.com>
2024-01-19 10:37:45 -0500

    Implement modularity @TestData
    Ref: [Clean code]

--- Budget/Models/TestData.swift
+++ Budget/Models/TestData.swift
@@ -8,26 +8,28 @@
 import Foundation
 
 struct TestData {
-    static let transactions: [Transaction] = [
-        Transaction(
-            amount: 200_000,
-            category: .income,
-            date: Date(),
-            description: "Salary"
-        ),
-        Transaction(
-            amount: -1_999,
-            category: .utilities,
-            date: Date(),
-            description: "Phone bill"
-        ),
-        Transaction(
-            amount: -5_687,
-            category: .groceries,
-            date: Date(),
-            description: "Food"
-        )
-    ]
+    static let salary = Transaction(
+        amount: Int.random(in: 100_000...1_000_000),
+        category: .income,
+        date: Date(),
+        description: "Salary"
+    )
+    
+    static let phoneBill = Transaction(
+        amount: Int.random(in: 100...20_000) * -1,
+        category: .utilities,
+        date: Date(),
+        description: "Phone bill"
+    )
+    
+    static let food = Transaction(
+        amount: Int.random(in: 100...20_000) * -1,
+        category: .groceries,
+        date: Date(),
+        description: "Food"
+    )
+    
+    static let transactions: [Transaction] = [salary, phoneBill, food]
     
     static let account = Account(transactions: transactions)
 }
